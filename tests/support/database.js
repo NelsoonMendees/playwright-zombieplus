6f00737eb76e4ebd497c0b9b9cf1573b3b8afdb6
Nelson Gomes <nelson_mendes@live.com>
2024-01-12 14:03:32 -0300

    Ajustes arquivo database.js e adicionando README.md

--- tests/support/database.js
+++ tests/support/database.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg')
 
 const DbConfig = {
-  user: 'vpznhysp',
-  host: 'berry.db.elephantsql.com',
-  database: 'vpznhysp',
-  password: 'm8F1P9WZDDbcPHgFN2y1RfYR8jKnizMT',
+  user: 'mipyddmd',
+  host: 'tuffi.db.elephantsql.com',
+  database: 'mipyddmd',
+  password: 'pE_Mb17lkzY72ySUgdf7mOa9vIbqmvmR',
   port: 5432
 }
 
